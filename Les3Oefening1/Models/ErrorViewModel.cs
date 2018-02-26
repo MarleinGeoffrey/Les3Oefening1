f080f32494e4c77471a3c1151f8bf1f6a3b5eebb
MarleinGeoffrey <geoffrey.marlein@student.howest.be>
2018-02-26 10:09:46 +0100

    Add project files.

---
+++ Les3Oefening1/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Les3Oefening1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
