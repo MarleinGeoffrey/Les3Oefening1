165085dd9a23d98739800c9e967bb1b02db8ffc1
MarleinGeoffrey <geoffrey.marlein@student.howest.be>
2018-02-26 10:11:59 +0100

    Added Item.cs

---
+++ Les3Oefening1/Models/Item.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Les3Oefening1.Models
+{
+    public class Item
+    {
+        public string Naam { get; set; }
+        public int Aantal { get; set; }
+    }
+}
